9537489bf23286d707324fd7b0fa91e05eaff8c6
maximkozhin24 <maximkozhin24@gmail.com>
2026-02-26 13:38:42 +0300

    init commit

--- src/main/java/com/logistics/logisticsapp/mapper/VehicleMapper.java
+++ src/main/java/com/logistics/logisticsapp/mapper/VehicleMapper.java
@@ -4,7 +4,7 @@ import com.logistics.logisticsapp.dto.VehicleDto;
 import com.logistics.logisticsapp.entity.Vehicle;
 
 public class VehicleMapper {
-
+    private VehicleMapper() {}
     public static VehicleDto toDto(Vehicle vehicle) {
         return new VehicleDto(
                 vehicle.getId(),
