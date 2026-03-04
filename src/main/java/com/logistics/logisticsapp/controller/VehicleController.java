4a15f77643a40675ae2a1a6cbd3a8e8c354e352e
maximkozhin24 <maximkozhin24@gmail.com>
2026-03-04 13:59:35 +0300

    sonar cloud fix

--- src/main/java/com/logistics/logisticsapp/controller/VehicleController.java
+++ src/main/java/com/logistics/logisticsapp/controller/VehicleController.java
@@ -2,7 +2,11 @@ package com.logistics.logisticsapp.controller;
 
 import com.logistics.logisticsapp.dto.VehicleDto;
 import com.logistics.logisticsapp.service.VehicleService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
--- src/test/java/com/logistics/logisticsapp/LogisticsAppApplicationTests.java
+++ src/test/java/com/logistics/logisticsapp/LogisticsAppApplicationTests.java
@@ -9,5 +9,4 @@ class LogisticsAppApplicationTests {
     @Test
     void contextLoads() {
     }
-
 }
