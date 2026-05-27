8b0349e23850acf4e3fe7ae9428f8d1631f61d45
maximkozhin24 <maximkozhin24@gmail.com>
2026-05-27 10:07:36 +0300

    lab 8 ci cd

--- src/test/java/com/logistics/logisticsapp/service/ApplicationTests.java
+++ src/test/java/com/logistics/logisticsapp/service/ApplicationTests.java
@@ -3,12 +3,14 @@ package com.logistics.logisticsapp.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-    @SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
     class ApplicationTests {
 
         @Test
         void contextLoads() {
+            assertThat(true).isTrue();
         }
-
     }
 
